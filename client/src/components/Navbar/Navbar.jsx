accdde5bde96d4c1c5f581bf3498a3b39ad0eb0f
9400749917megha <meghasaju45@gmail.com>
2025-04-10 12:25:26 +0530

    Add navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <img src={assets.logo} alt="" className='logo' />
+      <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile-app</li>
+        <li>Contact us</li>
+      </ul>
+
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search_icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+
+        <button>Sign in</button>
+      </div>
+    </div>
   )
 }
 
